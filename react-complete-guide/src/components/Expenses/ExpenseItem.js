5b620c4e3d9073b9762abe7783d1449a05ab6298
gabatk <gabriela.trybuszek@gmail.com>
2023-03-03 20:38:38 +0100

    clickHandler function changed.

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -2,20 +2,22 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
-const clickHandler = () => {
-	console.log('Clicked!');
-}
+const ExpenseItem = props => {
+	let title = props.title;
+
+	const clickHandler = () => {
+		console.log('Clicked!');
+	};
 
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 			</div>
 			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
-}
+};
 export default ExpenseItem;
