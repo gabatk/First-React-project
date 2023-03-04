e281b240fef63fa556b661f3023c1626322fca8d
gabatk <gabriela.trybuszek@gmail.com>
2023-03-04 17:18:47 +0100

    Expense form implemented.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 const expenses = [
 	{
@@ -25,7 +26,7 @@ const expenses = [
 const App = () => {
 	return (
 		<div>
-			<h2>Let's get started!</h2>
+			<NewExpense />
 			<Expenses items={expenses} />
 		</div>
 	);
