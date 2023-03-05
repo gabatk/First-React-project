54b4ea8c18359574e174b759ae6776b0244276cd
gabatk <gabriela.trybuszek@gmail.com>
2023-03-05 22:13:47 +0100

    Two-way binding implemented. Clear form.

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -54,6 +54,10 @@ const ExpenseForm = () => {
 		};
 
 		console.log(expenseData);
+		// Czyszczenie inputów dzięki two-way binding.
+		setEnteredTitle('');
+		setEnteredAmount('');
+		setEnteredDate('');
 	};
 
 	return (
@@ -61,7 +65,11 @@ const ExpenseForm = () => {
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type='text' onChange={titleChangeHandler} />
+					<input
+						type='text'
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
@@ -69,6 +77,7 @@ const ExpenseForm = () => {
 						type='number'
 						min='0.01'
 						step='0.01'
+						value={enteredAmount}
 						onChange={amountChangeHandler}
 					/>
 				</div>
@@ -78,6 +87,7 @@ const ExpenseForm = () => {
 						type='date'
 						min='2022-01-01'
 						max='2025-12-31'
+						value={enteredDate}
 						onChange={dateChangeHandler}
 					/>
 				</div>
